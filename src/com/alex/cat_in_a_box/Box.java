5b1afaa0f4e793f487fa44244a66ddd9af415774
User <aliaksandr1302@gmail.com>
2020-02-27 14:28:40 +0300

    threads homework

--- src/com/alex/cat_in_a_box/Box.java
+++ src/com/alex/cat_in_a_box/Box.java
@@ -1,4 +1,8 @@
 package com.alex.cat_in_a_box;
 
 public class Box {
+    private boolean status;
+    public Box(Cat cat){
+
+    }
 }
--- src/com/alex/cat_in_a_box/Cat.java
+++ src/com/alex/cat_in_a_box/Cat.java
@@ -1,5 +1,25 @@
 package com.alex.cat_in_a_box;
 
 public class Cat {
+    private String name;
+    private boolean alive = true;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public Cat(String name, boolean alive) {
+        this.name = name;
+        this.alive = alive;
+    }
 
 }
